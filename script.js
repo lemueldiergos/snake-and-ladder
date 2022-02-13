8472d4545fa589a80198ce2b21166b988b03b5ff
lemueldiergos <diergoslemuel12@gmail.com>
2022-02-13 16:23:44 +0800

    add hmtl and js

---
+++ script.js
@@ -0,0 +1,13 @@
+var cvs = document.getElementById("cvs");
+var ctx = cvs.getContext("2d");
+var board_width = window.innerWidth/2;
+var board_height = window.innerHeight/2;
+
+with(cvs) {
+    width = board_width;
+    height = board_width;
+}
+
+with(ctx) {
+    
+}

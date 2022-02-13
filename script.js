560ff3cadf7537eaf120abce132215753c1ce20d
lemueldiergos <diergoslemuel12@gmail.com>
2022-02-13 16:32:51 +0800

    updates

--- script.js
+++ script.js
@@ -8,6 +8,19 @@ with(cvs) {
     height = board_width;
 }
 
+var box_size = board_width/10;
+var box_margin = box_size*0.05;
+
 with(ctx) {
-    
+    fillStyle = "#764ba2";
+    for(y = 0; y < 10 ; y++) {    
+        for(x = 0; x < 10 ; x++) {
+            fillRect(
+                x * box_size,
+                y * box_size,
+                box_size-box_margin,
+                box_size-box_margin
+            );
+        }
+    }
 }

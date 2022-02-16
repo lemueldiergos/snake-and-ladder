dea23f5f3712cdc62442089a35c48241456a13d2
lemueldiergos <diergoslemuel12@gmail.com>
2022-02-16 12:33:48 +0800

    update (adding snake)

--- script.js
+++ script.js
@@ -7,18 +7,19 @@ var dice_dots = document.getElementsByClassName("dots");
 var board_width = window.innerWidth/2.5;
 var board_height = window.innerHeight/2.5;
 
+var on_movement_state = false;
 
 var map = [
-    0, 0, 0, 2, 0, 2, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 2, 0, 0,
-    0, 0, 3, 0, 0, 0, 0, 0, 0, 0,
-    0, 2, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 3, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 0, 0, 2, 0, 0,
-    0, 0, 0, 0, 2, 0, 0, 0, 2, 0,
-    0, 0, 0, 3, 0, 3, 0, 0, 0, 0,
-    0, 0, 3, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0, 0, 3, 0, 0, 0, 3
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 ]
 
 
@@ -58,9 +59,27 @@ var map_render =()=> {
                 );
             }
         }
+        //x - y
+        // snake length - percentage
+        var snake_head = [
+            [85, 75, 45, 15, 35, 75, 55],
+            [65, 55, 65, 35,  5, 15, 5]
+        ];
+        var snake_tail = [
+            [95, 55, 55, 25, 25, 35, 45],
+            [95, 75, 95, 85, 25, 75, 45]
+        ];
+        for(i=0;i<snake_head[0].length;i++) {
+            beginPath();
+                lineWidth = 4;
+                strokeStyle = "cornflowerblue";
+                moveTo(board_width*(snake_head[0][i]/100), board_width*(snake_head[1][i]/100));
+                lineTo(board_width*(snake_tail[0][i]/100), board_width*(snake_tail[1][i]/100));
+                stroke();
+            closePath();
+        }
     }    
 }
-
 map_render();
 
 var x_inc =             0,
@@ -96,7 +115,7 @@ var snake_catcher=(xs_in=0, ys_in=0, os_in=0, is_in=0, ii_in=0, fs_in=0)=> {
     const is         = [false, false, true, true, true, false, false];
     const ii         = [30, 30, 40, 60, 80, 90, 90];
     const fs         = [68, 64, 57,  31, 17, 5, 3];
-    //10 6 26 18 24 56 78
+    
     const out_xs     = [10, 6, 6, 8, 4, 6, 8];
     const out_ys     = [1, 5, 5, 8, 7, 6, 8];
     const out_os     = [19, 11, 11, 15, 7, 11, 15];
@@ -115,16 +134,25 @@ var snake_catcher=(xs_in=0, ys_in=0, os_in=0, is_in=0, ii_in=0, fs_in=0)=> {
         ii_in == ii[i] &&  
         fs_in == fs[i]  
       ) {
-        
-        x_inc =             out_xs[i],
-        y_inc =             out_ys[i],
-        odd_num =           out_os[i],
-        inversion_state =   out_is[i],
-        increment_by_10 =   out_ii[i],
+        map[final_movement] = 0;
+        x_inc =             out_xs[i];
+        y_inc =             out_ys[i];
+        odd_num =           out_os[i];
+        inversion_state =   out_is[i];
+        increment_by_10 =   out_ii[i];
         final_movement =    out_fs[i];
-
-      }
-      map_render();
+        map[final_movement] = 1;
+        map_render();
+        console.log(
+            " " + x_inc
+           +" " + y_inc
+           +" " +odd_num
+           +" " +inversion_state
+           +" " +increment_by_10
+           +" " + (final_movement)
+       );
+        
+        }
     }
 }
 
@@ -151,18 +179,26 @@ var Roll =()=> {
     }
     final_movement = 100-(y_inc + increment_by_10)
     map[final_movement] = 1;
-   
+    //console.log(final_movement);
+
+    var second_ver_head = [68, 64, 57, 31, 17,  5,  3];
+    var second_ver_tail = [99, 95, 75, 82, 73, 44, 22];
+    //console.log(second_ver_head.length);
+
+    for(i = 0 ; i < second_ver_head.length ;i++) {
+        if(final_movement == second_ver_head[i] && on_movement_state == true) {
+            map[second_ver_tail[i]] = 1;
+            snake_catcher(
+                x_inc,
+                y_inc,
+                odd_num,
+                inversion_state,
+                increment_by_10,
+                final_movement 
+            );
+        }
+    }
     map_render();
-    
-    console.log(
-         " " + x_inc
-        +" " + y_inc
-        +" " +odd_num
-        +" " +inversion_state
-        +" " +increment_by_10
-        +" " + (final_movement)
-    );
-  // console.log(x_inc);
 }
 
 var c_a_time =      250,
@@ -172,7 +208,6 @@ var c_a_time =      250,
 var previous_span_count = 0;
 
 var rolling=()=> {
-    
     previous_span_count = c_a_limit;
     c_a_limit = Math.ceil(Math.random()*6);
     dice_roll.innerHTML = c_a_limit;
@@ -185,36 +220,26 @@ var roll_ctrl=()=> {
     
     c_a_counter += 1;
     if(c_a_counter == c_a_limit) {
-       
+       on_movement_state = true;
         clearTimeout(roll_ctrl);
         dice_body.disabled = false;
-        snake_catcher(
-            x_inc,
-            y_inc,
-            odd_num,
-            inversion_state,
-            increment_by_10,
-            final_movement 
-           );
+           
     }
     else {
+        on_movement_state = false;
         setTimeout(roll_ctrl, c_a_time/2);
     }
     Roll();
 }
 
+
+// for debugging only
 var loop=()=> {
+  //  map_render();
     requestAnimationFrame(loop);
 }
 requestAnimationFrame(loop);
 
-// var loop =()=> {
-//     Roll();
-//     //console.log(final_movement);
-//     if(final_movement == -10) cancelAnimationFrame(loop);
-//     else requestAnimationFrame(loop);
-// }
-// requestAnimationFrame(loop);
 
 
 
